0916e583f1dee77f1454bc2adabec80fb81f1c89
AntoRin_ux <antorinish@gmail.com>
2021-08-26 11:47:36 +0530

    hook change

--- webHooks.js
+++ webHooks.js
@@ -6,10 +6,11 @@ router.get("/personal/secret/ms/bot/webhook", (req, res) => {
    console.log(req.body);
 });
 
-router.get("/personal/secret/ms/drive/webhook", (req, res) => {
+router.post("/personal/secret/ms/drive/webhook", (req, res) => {
    const { validationToken } = req.query;
    const decodedToken = decodeURIComponent(validationToken);
    console.log(req.body);
+   res.setHeader("Content-Type", "text/plain");
    res.send(decodedToken);
 });
 

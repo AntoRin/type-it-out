84618e55d5d1fd23ad2d19e9eb652073f31f2c5b
AntoRin_ux <antorinish@gmail.com>
2021-08-26 10:36:30 +0530

    add webhook confirm response

--- webHooks.js
+++ webHooks.js
@@ -8,6 +8,7 @@ router.get("/personal/secret/ms/bot/webhook", (req, res) => {
 
 router.get("/personal/secret/ms/drive/webhook", (req, res) => {
    console.log(req.body);
+   res.status(200).end();
 });
 
 module.exports = { webHookRouter: router };

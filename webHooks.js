815b9502b51a279e6902084be269e277cfff63d2
AntoRin_ux <antorinish@gmail.com>
2021-08-29 13:11:12 +0530

    fix bug

--- webHooks.js
+++ webHooks.js
@@ -47,7 +47,7 @@ router.post("/personal/secret/ms/chat/webhook", async (req, res) => {
          {
             key: process.env.PRIVATE_KEY,
          },
-         encryptedData
+         Buffer.from(encryptedData)
       );
 
       console.log("Decrypted Data: ", decryptedData.toString());

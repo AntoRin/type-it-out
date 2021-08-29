066e9ddc3df26b17d0931997815b24e2ea3792d2
AntoRin_ux <antorinish@gmail.com>
2021-08-29 13:02:55 +0530

    add decryption

--- webHooks.js
+++ webHooks.js
@@ -19,12 +19,13 @@ router.post("/personal/secret/ms/drive/webhook", (req, res) => {
       }
 
       console.log(req.body);
+      res.status(200).end();
    } catch (error) {
       res.status(200).end();
    }
 });
 
-router.post("/personal/secret/ms/chat/webhook", (req, res) => {
+router.post("/personal/secret/ms/chat/webhook", async (req, res) => {
    try {
       const { validationToken } = req.query;
       if (validationToken) {
@@ -36,9 +37,23 @@ router.post("/personal/secret/ms/chat/webhook", (req, res) => {
 
       const { value, validationTokens } = req.body;
 
-      console.log("Resource Data", value[0].resourceData);
-      console.log("Encryption Content", value[0].encryptedContent);
+      // console.log("Resource Data", value[0].resourceData);
+      // console.log("Encryption Content", value[0].encryptedContent);
+
+      const encryptedData = value[0].encryptedContent.data;
+
+      const decryptedData = crypto.privateDecrypt(
+         {
+            key: process.env.PRIVATE_KEY,
+         },
+         encryptedData
+      );
+
+      console.log("Decrypted Data: ", decryptedData.toString());
+
+      res.status(200).end();
    } catch (error) {
+      console.log(error);
       res.status(200).end();
    }
 });

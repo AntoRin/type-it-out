439691838ec54f2182e726c2b15eec3808d593d3
AntoRin_ux <antorinish@gmail.com>
2021-08-29 12:42:36 +0530

    add chat web hook

--- webHooks.js
+++ webHooks.js
@@ -9,11 +9,38 @@ router.post("/personal/secret/ms/bot/webhook", (req, res) => {
 });
 
 router.post("/personal/secret/ms/drive/webhook", (req, res) => {
-   const { validationToken } = req.query;
-   const decodedToken = decodeURIComponent(validationToken);
-   console.log(req.body);
-   res.setHeader("Content-Type", "text/plain");
-   res.send(decodedToken);
+   try {
+      const { validationToken } = req.query;
+      if (validationToken) {
+         const decodedToken = decodeURIComponent(validationToken);
+         res.setHeader("Content-Type", "text/plain");
+         res.send(decodedToken);
+         return;
+      }
+
+      console.log(req.body);
+   } catch (error) {
+      res.status(200).end();
+   }
+});
+
+router.post("/personal/secret/ms/chat/webhook", (req, res) => {
+   try {
+      const { validationToken } = req.query;
+      if (validationToken) {
+         const decodedToken = decodeURIComponent(validationToken);
+         res.setHeader("Content-Type", "text/plain");
+         res.send(decodedToken);
+         return;
+      }
+
+      const { value, validationTokens } = req.body;
+
+      console.log("Resource Data", value[0].resourceData);
+      console.log("Encryption Content", value[0].encryptedContent);
+   } catch (error) {
+      res.status(200).end();
+   }
 });
 
 module.exports = { webHookRouter: router };
